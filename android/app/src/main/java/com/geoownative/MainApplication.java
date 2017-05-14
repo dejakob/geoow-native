e0875fc3a04c8af9a65210f74f4109a1fea6220e
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-05-14 20:08:09 +0200

    Add account kit config

--- android/app/src/main/java/com/geoownative/MainApplication.java
+++ android/app/src/main/java/com/geoownative/MainApplication.java
@@ -3,6 +3,7 @@ package com.geoownative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAccountKitPackage(),
             new VectorIconsPackage()
       );
     }
