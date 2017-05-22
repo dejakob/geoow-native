8b2ec6a8cb483676b70d23a191174f3e35305577
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-05-22 22:12:02 +0200

    Add createQuest API call

---
+++ api/quest.js
@@ -0,0 +1,28 @@
+import store from '../services/store';
+import { getCurrentToken } from '../sagas/auth';
+import { API } from '../constants';
+
+const API_URL = API.URL;
+
+function createQuest(event) {
+    const locationState = store.getState().location;
+    const location = [ locationState.get('longitude'), locationState.get('latitude') ];
+
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${getCurrentToken()}`
+        },
+        body: JSON.stringify({
+            event,
+            location
+        })
+    };
+
+    return fetch(`${API_URL}/user/me/categories`, params);
+}
+
+export {
+    createQuest
+}
