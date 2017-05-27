5b7755487259713cf087a858f096504483e52a69
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-05-27 22:24:29 +0200

    dashboard list styling

--- components/dashboard-list/dashboard-list-item.js
+++ components/dashboard-list/dashboard-list-item.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { getStyle } from 'react-native-styler';
 import { getDescription } from '../../helpers/user-stats-helper';
 
@@ -11,10 +11,32 @@ import { getDescription } from '../../helpers/user-stats-helper';
 function DashboardListItem(props) {
     const { item } = props;
 
+    const overlayStyle = item.score > 0 ?
+        getStyle('dashboard__listItem__header__overlay__ok') :
+        getStyle('dashboard__listItem__header__overlay__notok')
+
     return (
         <View
             style={getStyle('dashboard__listItem')}
         >
+            <View
+                style={getStyle('dashboard__listItem__header')}
+            >
+                <Image
+                    style={getStyle('dashboard__listItem__header__image')}
+                    source={require('../../assets/beach-pexels.jpeg')}
+                    resizeMode='cover'
+                />
+                <View
+                    style={[getStyle('dashboard__listItem__header__overlay'), overlayStyle]}
+                >
+                    <Text
+                        style={getStyle('dashboard__listItem__header__score')}
+                    >
+                        {item.score}
+                    </Text>
+                </View>
+            </View>
             <View
                 style={getStyle('dashboard__listItem__descriptionContainer')}
             >
--- components/dashboard-list/dashboard-list.style.js
+++ components/dashboard-list/dashboard-list.style.js
@@ -7,6 +7,8 @@ createStyle({
             flexDirection: 'row',
             flex: 1,
             paddingTop: '30h4s',
+            paddingLeft: '4h4s',
+            paddingRight: '4h4s',
 
             header: {
                 paddingTop: '40h4s'
@@ -20,18 +22,53 @@ createStyle({
         listItem: {
             backgroundColor: 'theme:sheet',
             borderRadius: '3h4s',
+            overflow: 'hidden',
             margin: '4h4s',
 
+            header: {
+                position: 'relative',
+                height: '100h4s',
+
+                image: {
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    height: '100h4s',
+                    width: '100%'
+                },
+                overlay: {
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+
+                    ok: {
+                        backgroundColor: 'theme:okOpacity'
+                    },
+                    notok: {
+                        backgroundColor: 'theme:notokOpacity'
+                    }
+                },
+                score: {
+                    color: 'theme:sheet',
+                    fontSize: '36h4s',
+                    fontWeight: '100'
+                }
+            },
             date: {
                 fontSize: 'theme:small',
                 fontWeight: '600',
-                fontStyle: 'italic',
-                textAlign: 'right',
+                textAlign: 'center',
                 color: 'theme:subtext',
+                marginBottom: '8h4s'
             },
             descriptionContainer: {
                 backgroundColor: 'theme:lighten8',
-                padding: '4w4s',
+                paddingLeft: '4w4s',
+                paddingRight: '4w4s',
+                paddingTop: '8h4s',
+                paddingBottom: '8h4s',
                 borderRadius: '2h4s'
             },
             description: {
