cf1fc8a163c96a9105d14552075fa5cb641914d7
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-08-07 23:28:08 +0200

    Upgrade to RN 0.47.1

--- ios/build/Build/Intermediates/RNBackgroundGeolocation.build/Debug-iphonesimulator/RNBackgroundGeolocation.build/Objects-normal/x86_64/RNBackgroundGeolocation.d
+++ ios/build/Build/Intermediates/RNBackgroundGeolocation.build/Debug-iphonesimulator/RNBackgroundGeolocation.build/Objects-normal/x86_64/RNBackgroundGeolocation.d
@@ -2,7 +2,15 @@ dependencies: \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/RNBackgroundGeolocation.m \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/RNBackgroundGeolocation.h \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Modules/module.modulemap \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSGeofenceEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSGeofencesChangeEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSScheduleEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSHeartbeatEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSHttpEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSProviderChangeEvent.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSActivityChangeEvent.h \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/LocationManager.h \
+  /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSLocation.h \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSSchedule.h \
   /Users/jakobkerkhove/geoow/geoow-native/node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/TSLocationManager.framework/Headers/TSLocationManager.h \
   ../../react-native/React/Modules/RCTEventEmitter.h \
@@ -13,4 +21,4 @@ dependencies: \
   /Users/jakobkerkhove/geoow/geoow-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/jakobkerkhove/geoow/geoow-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
   /Users/jakobkerkhove/geoow/geoow-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
-  ../../react-native/React/Base/RCTEventDispatcher.h
+  ../../react-native/React/Base/RCTInvalidating.h
