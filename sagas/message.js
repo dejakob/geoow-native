612df610e0daa28362d14530032da185bb262f96
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-08-20 19:54:08 +0200

    message api and saga

--- sagas/message.js
+++ sagas/message.js
@@ -1,11 +1,22 @@
 import Actions from '../actions';
 import { call, put } from 'redux-saga/effects';
 
+import * as MessageApi from '../api/message';
+
 function* loadMessages(action) {
     try {
         const { user, venue } = action;
 
-        yield put(Actions.loadMessagesSuccess());
+        let result = null;
+
+        if (user) {
+            result = yield call(MessageApi.loadUserMessages, user);
+        }
+        else {
+            result = yield call(MessageApi.loadVenueMessages, venue);
+        }
+
+        yield put(Actions.loadMessagesSuccess(result));
     }
     catch (ex) {
         yield put(Actions.loadMessagesFailed());
@@ -15,6 +26,13 @@ function* loadMessages(action) {
 function* sendMessage(action) {
     try {
         const { user, venue, body } = action;
+
+        if (user) {
+            yield call(MessageApi.sendMessageToUser, user, body);
+        }
+        else {
+            yield call(MessageApi.sendMessageToVenue, venue, body);
+        }
 
         yield put(Actions.sendMessageSuccess());
     }
